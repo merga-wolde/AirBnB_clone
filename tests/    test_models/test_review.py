d071009d12580780620bd717736cd0f1c2535940
Edemialem <136074891+Edemialem21@users.noreply.github.com>
2024-02-11 20:24:14 +0300

    test_review.py

--- tests/    test_models/test_review.py
+++ tests/    test_models/test_review.py
@@ -3,9 +3,7 @@
 """
 File: test_review.py
 Desc: This module contains all possible testcases for the review.py
-      modlue in the models package. It uses the standard unittest.
-Author: Gizachew Bayness (Elec Crazy) and Biruk Gelelcha
-Date Created: Sep 3 2022
+      modlue in the models package. It uses the standard
 """
 import unittest
 import models
